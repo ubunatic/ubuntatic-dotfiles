6387d37f2d706131e6c70d37403945fdc4295ee0
Uwe Jugel <uwe.jugel@lovoo.com>
2016-08-02 19:39:06 +0200

    adding python urlencode

--- .shellib/soundboard.sh
+++ .shellib/soundboard.sh
@@ -1,6 +1,11 @@
+# Please set your SOUNDBOARD_CMD in your ~/.profile
+
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+
 soundboard() {
-	test -z $SOUNDBOARD_CMD    && SOUNDBOARD_CMD="curl"
-	test -z $SOUNDBOARD_PREFIX && SOUNDBOARD_PREFIX="localhost:8080/play/"
+	test -z "$SOUNDBOARD_CMD"    && SOUNDBOARD_CMD="curl"
+	test -z "$SOUNDBOARD_PREFIX" && SOUNDBOARD_PREFIX="localhost:8080/play/"
 	cwd=$PWD
 	debug "soundboard args: '$@'"
 	if echo "$@" | grep '\.\(mp3\|wav\|mp4\)$'
@@ -11,14 +16,15 @@ soundboard() {
 	then
 		debug "playing regex"
 		cd $SOUNDBOARD_PATH
-		file=`find | grep "$@" | head -n1`
+		file=`find | grep -i "$@" | head -n1`
 		cd $cwd
 	else 
 		error "SOUNDBOARD_PATH:$SOUNDBOARD_PATH not found"
 	fi
-	file=`echo "$file" | sed 's/ /%20/g'`
-	echo "$SOUNDBOARD_CMD $SOUNDBOARD_PREFIX$file"
-	$SOUNDBOARD_CMD $SOUNDBOARD_PREFIX$file
+	#file=`echo "$file" | sed 's/ /%20/g'`
+	file=`urlencode "$file"`
+	echo "$SOUNDBOARD_CMD $SOUNDBOARD_OPTS $SOUNDBOARD_PREFIX$file"
+	$SOUNDBOARD_CMD $SOUNDBOARD_OPTS "$SOUNDBOARD_PREFIX$file"
 }
 alias play='soundboard'
 
